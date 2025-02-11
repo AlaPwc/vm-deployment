ad9182780f30f6f285d75d67d5e2b088262125b7
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-11 15:54:05 +0100

    Update lambda.tf

--- modules/AWS/ag/lambda.tf
+++ modules/AWS/ag/lambda.tf
@@ -39,3 +39,58 @@ resource "aws_iam_role" "lambda_exec" {
 }
 EOF
 }
+
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = "${aws_api_gateway_rest_api.example-mcloudshowcase.id}"
+  parent_id   = "${aws_api_gateway_rest_api.example-mcloudshowcase.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = "${aws_api_gateway_rest_api.example-mcloudshowcase.id}"
+  resource_id   = "${aws_api_gateway_resource.proxy.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.example-mcloudshowcase.id}"
+  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.example-mcloudshowcase.invoke_arn}"
+}
+
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = "${aws_api_gateway_rest_api.example-mcloudshowcase.id}"
+  resource_id   = "${aws_api_gateway_rest_api.example-mcloudshowcase.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = "${aws_api_gateway_rest_api.example-mcloudshowcase.id}"
+  resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+  http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.example-mcloudshowcase.invoke_arn}"
+}
+
+
+resource "aws_api_gateway_deployment" "example-mcloudshowcase" {
+  depends_on = [
+    "aws_api_gateway_integration.lambda",
+    "aws_api_gateway_integration.lambda_root",
+  ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.example-mcloudshowcase.id}"
+  stage_name  = "test"
+}
+
+
+
