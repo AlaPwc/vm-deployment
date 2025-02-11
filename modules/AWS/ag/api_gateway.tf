99ab27b766694029a1ccac0a2914d77b23661fd9
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-11 15:56:52 +0100

    Update api_gateway.tf

--- modules/AWS/ag/api_gateway.tf
+++ modules/AWS/ag/api_gateway.tf
@@ -2,3 +2,7 @@ resource "aws_api_gateway_rest_api" "example-mcloudshowcase" {
   name        = "ServerlessExample-mcloudshowcase"
   description = "Terraform Serverless Application Example"
 }
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.example.invoke_url}"
+}
