b4e059c02b162c337086c71283bd97900d3d7ab8
Ala Shaban <ala.eddin.mahmoud.shaban@pwc.com>
2025-02-12 17:08:40 +0000

    add google vm

---
+++ modules/Google/main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+  credentials = file("gcp-key.json")
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-github-action-vm"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11" # Free-tier eligible image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    startup-script = <<-EOT
+      #!/bin/bash
+      echo "Hello, Terraform from GitHub Actions!" > /var/www/html/index.html
+    EOT
+  }
+}
