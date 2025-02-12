476beabbd67dd5d07b3226f7480fd2ed31fd9134
Ala Shaban <ala.eddin.mahmoud.shaban@pwc.com>
2025-02-12 18:32:31 +0000

    fix

--- modules/Google/variables.tf
+++ modules/Google/variables.tf
@@ -14,5 +14,5 @@ variable "zone" {
 variable "GOOGLE_CREDENTIALS" {
   description = "Dummy variable for Google Cloud credentials"
   type        = string
-  default     = ""  # Provide an empty default value since this will be overridden by the environment variable
+  default     = "" # Provide an empty default value since this will be overridden by the environment variable
 }
