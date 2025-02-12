073b4fd82a9fb504623ff5fe609f66a27c070983
Ala Shaban <ala.eddin.mahmoud.shaban@pwc.com>
2025-02-12 18:23:54 +0000

    GCP VM

--- modules/Google/variables.tf
+++ modules/Google/variables.tf
@@ -11,3 +11,8 @@ variable "zone" {
   description = "GCP Zone"
   default     = "europe-west10-a"
 }
+variable "GOOGLE_CREDENTIALS" {
+  description = "Dummy variable for Google Cloud credentials"
+  type        = string
+  default     = ""  # Provide an empty default value since this will be overridden by the environment variable
+}
