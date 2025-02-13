6ea4b3b779e598af36958e636f66ecd1af626822
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-13 13:19:53 +0100

    Update lambda.tf

--- modules/AWS/ag/lambda.tf
+++ modules/AWS/ag/lambda.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "example-mcloudshowcase" {
 # IAM role which dictates what other AWS services the Lambda function
 # may access.
 resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_example_lambda-mcloudshowcase"
+  name = "lambda-mcloudshowcase"
 
   assume_role_policy = <<EOF
 {
