ba012274606231af4b76d300b8b3d326840ab8d0
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-13 14:42:58 +0100

    Update lambda.tf

--- modules/AWS/ag/lambda.tf
+++ modules/AWS/ag/lambda.tf
@@ -17,8 +17,30 @@ resource "aws_lambda_function" "example-mcloudshowcase" {
   handler = "main.handler"
   runtime = "nodejs10.x"
 
- # role = "${aws_iam_role.lambda_exec.arn}"
+  role = "${aws_iam_role.lambda_exec_mcloud_showcase.arn}"
 }
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+
+resource "aws_iam_role" "lambda_exec_mcloud_showcase" {
+  name = "lambda-mcloudshowcase"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 
 
 resource "aws_api_gateway_resource" "proxy" {
