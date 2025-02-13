8e6a1bca0f4cf42f59055e6925b7540080890f09
Ala Shaban <ala.eddin.mahmoud.shaban@pwc.com>
2025-02-13 17:33:41 +0000

    fix output

--- modules/Google/storage/outputs.tf
+++ modules/Google/storage/outputs.tf
@@ -1,3 +1,20 @@
-output "vm_public_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+
+output "bucket_name" {
+  description = "The name of the storage bucket"
+  value       = google_storage_bucket.my_bucket.name
+}
+
+output "bucket_self_link" {
+  description = "The self-link of the storage bucket"
+  value       = google_storage_bucket.my_bucket.self_link
+}
+
+output "bucket_url" {
+  description = "The public URL of the storage bucket"
+  value       = "https://storage.googleapis.com/${google_storage_bucket.my_bucket.name}"
+}
+
+output "bucket_location" {
+  description = "The region of the storage bucket"
+  value       = google_storage_bucket.my_bucket.location
 }
