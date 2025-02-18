aab0853f45bf4c881f002fc3dd9a7567f4a2efdb
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-18 13:27:19 +0100

    Update main.tf

--- modules/AWS/website/main.tf
+++ modules/AWS/website/main.tf
@@ -37,23 +37,6 @@ resource "aws_s3_bucket_versioning" "versioning_mcloud-showcase" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership-mcloud-showcase" {
-  bucket = data.aws_s3_bucket.selected-bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-  depends_on = [aws_s3_bucket_public_access_block.mcloud-showcase]
-}
-
-resource "aws_s3_bucket_public_access_block" "mcloud-showcase" {
-  bucket = data.aws_s3_bucket.selected-bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_policy" "bucket-policy-mcloud-showcase" {
   bucket = data.aws_s3_bucket.selected-bucket.id
   policy = data.aws_iam_policy_document.iam-policy-mcloud-showcase.json
@@ -75,6 +58,24 @@ principals {
   }
 
   depends_on = [aws_s3_bucket_public_access_block.mcloud-showcase]
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership-mcloud-showcase" {
+  bucket = data.aws_s3_bucket.selected-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.mcloud-showcase]
+}
+
+resource "aws_s3_bucket_public_access_block" "mcloud-showcase" {
+  bucket = data.aws_s3_bucket.selected-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "website-config" {
