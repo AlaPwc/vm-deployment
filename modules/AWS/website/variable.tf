da80ce8a8aabbcd00600cb1548f56367a6714806
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-18 12:22:05 +0100

    Update variable.tf

--- modules/AWS/website/variable.tf
+++ modules/AWS/website/variable.tf
@@ -1,9 +1,12 @@
 variable "domain_name" {
   type = string
   description = "Name of the domain"
+  default = "mcloud-showcase"
 }
+
 variable "bucket_name" {
   type = string
-  description = "Name of the bucket."
+  description = "Name of the bucket"
+  default = "mcloud-showcase-bucket"
 }
 
