f84401bbedd0c9216c4f57017f73a9ea176b99ca
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-18 12:41:47 +0100

    Update s3-bucket.tf

--- modules/AWS/website/s3-bucket.tf
+++ modules/AWS/website/s3-bucket.tf
@@ -79,12 +79,12 @@ routing_rule {
 }
 
 resource "aws_s3_object" "object-upload-html" {
-    for_each        = fileset("uploads/", "*.html")
+    for_each        = fileset("./", "*.html")
     bucket          = data.aws_s3_bucket.selected-bucket.bucket
     key             = each.value
     source          = "uploads/${each.value}"
     content_type    = "text/html"
-    etag            = filemd5("uploads/${each.value}")
+    etag            = filemd5("./${each.value}")
     acl             = "public-read"
 }
 
