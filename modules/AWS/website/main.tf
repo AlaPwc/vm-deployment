e67081ec869929b36e91a761e9d5f5a26cb131fc
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-18 12:01:24 +0100

    Update main.tf

--- modules/AWS/website/main.tf
+++ modules/AWS/website/main.tf
@@ -1,13 +1,18 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.2.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
+
+#test
+
+# Configure the AWS Provider
 provider "aws" {
-    access_key = var.access_key
-    secret_key = var.secret_key
-    region     = var.region
+  region = "eu-central-1"
 }
+
