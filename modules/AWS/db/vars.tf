8842890dffcf0dda2e48a0da10fe6ea5749b0856
bianguetinda <112000372+bianguetinda@users.noreply.github.com>
2025-02-19 13:09:51 +0100

    Update vars.tf

--- modules/AWS/db/vars.tf
+++ modules/AWS/db/vars.tf
@@ -1,1 +1,11 @@
+variable "cluster_name" {
+  type = string
+  description = "Name of the Aurora Cluster"
+  default = "Aurora-mcloud-showcase-cluster"
+}
 
+variable "cluster_instance_name" {
+  type = string
+  description = "Name of the Aurora Cluster Instance"
+  default = "Aurora-mcloud-showcase-cluster-instance"
+}
